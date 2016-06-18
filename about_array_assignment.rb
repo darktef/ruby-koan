ffe395c098b2b502fdd531c800c2b2babf06307b
darktef <darktef@gmail.com>
2016-06-17 21:23:06 -0500

    strings now"

--- about_array_assignment.rb
+++ about_array_assignment.rb
@@ -3,49 +3,55 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
     names = ["John", "Smith"]
-    assert_equal __, names
+    assert_equal ["John", "Smith"], names
   end
 
   def test_parallel_assignments
     first_name, last_name = ["John", "Smith"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_extra_values
     first_name, last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal "Smith", last_name
   end
 
   def test_parallel_assignments_with_splat_operator
     first_name, *last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name
+    assert_equal ["Smith", "III"], last_name
+    # *, splat _oprator
+    # will capture whatever left on the table...the rest of arguments or values or objects
+    # More readings, https://endofline.wordpress.com/2011/01/21/the-strange-ruby-splat/
   end
 
   def test_parallel_assignments_with_too_few_variables
     first_name, last_name = ["Cher"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "Cher", first_name
+    assert_equal nil, last_name
+    # if there is nothing, go for nil
   end
 
   def test_parallel_assignments_with_subarrays
     first_name, last_name = [["Willie", "Rae"], "Johnson"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal ["Willie", "Rae"], first_name
+    assert_equal "Johnson", last_name
+    # think in object
   end
 
   def test_parallel_assignment_with_one_variable
     first_name, = ["John", "Smith"]
-    assert_equal __, first_name
+    assert_equal "John", first_name
+    # notice the comma there
   end
 
   def test_swapping_with_parallel_assignment
     first_name = "Roy"
     last_name = "Rob"
     first_name, last_name = last_name, first_name
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "Rob", first_name
+    assert_equal "Roy", last_name
   end
 end
